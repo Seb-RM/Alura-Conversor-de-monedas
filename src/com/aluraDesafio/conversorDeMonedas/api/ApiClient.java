6ac1235857b75473d1c0eb9c9d4085cc7423f02e
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-10-08 21:03:58 -0300

    conexion http Api-client realizada

--- src/com/aluraDesafio/conversorDeMonedas/api/ApiClient.java
+++ src/com/aluraDesafio/conversorDeMonedas/api/ApiClient.java
@@ -1,4 +1,39 @@
 package com.aluraDesafio.conversorDeMonedas.api;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ApiClient {
+
+    private static final String API_URL = "https://v6.exchangerate-api.com/v6/";
+    private static final String ACCESS_KEY = "f9c94e16da0cb63888ce5348";
+
+    public String obtenerTasasDeCambio() {
+
+        String url = API_URL  + ACCESS_KEY + "/latest/USD";
+
+        URI direccion = URI.create(url);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                return response.body();
+            } else {
+                System.out.println("Error en la solicitud, estado: " + response.statusCode());
+            }
+        } catch(Exception e) {
+            System.out.println("Error al conectar a la API: " + e.getMessage());
+        }
+
+        return null;
+    }
 }
--- src/com/aluraDesafio/conversorDeMonedas/ui/Principal.java
+++ src/com/aluraDesafio/conversorDeMonedas/ui/Principal.java
@@ -1,4 +1,19 @@
 package com.aluraDesafio.conversorDeMonedas.ui;
 
+import com.aluraDesafio.conversorDeMonedas.api.ApiClient;
+
 public class Principal {
+
+    public static void main(String[] args) {
+
+        ApiClient apiClient = new ApiClient();
+
+        String resultado = apiClient.obtenerTasasDeCambio();
+
+        if (resultado != null) {
+            System.out.println("Datos de la API: " + resultado);
+        } else {
+            System.out.println("Error al obtener los datos de la API.");
+        }
+    }
 }
