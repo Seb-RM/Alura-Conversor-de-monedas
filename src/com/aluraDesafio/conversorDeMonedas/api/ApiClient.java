dde9596a3426013d969d34c452824eba8ee5b917
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-10-08 22:24:45 -0300

    definicion de variables de entorno

--- src/com/aluraDesafio/conversorDeMonedas/api/ApiClient.java
+++ src/com/aluraDesafio/conversorDeMonedas/api/ApiClient.java
@@ -1,18 +1,32 @@
 package com.aluraDesafio.conversorDeMonedas.api;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Properties;
 
 public class ApiClient {
 
     private static final String API_URL = "https://v6.exchangerate-api.com/v6/";
-    private static final String ACCESS_KEY = "f9c94e16da0cb63888ce5348";
+    private String apiKey;
+
+    public ApiClient() {
+        Properties properties = new Properties();
+        try {
+            FileInputStream fileInputStream = new FileInputStream("src/config.properties");
+            properties.load(fileInputStream);
+            this.apiKey = properties.getProperty("API_KEY");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public String obtenerTasasDeCambio() {
 
-        String url = API_URL  + ACCESS_KEY + "/latest/USD";
+        String url = API_URL  + apiKey + "/latest/USD";
 
         URI direccion = URI.create(url);
 
